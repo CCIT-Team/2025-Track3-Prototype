2cf9a2fae62ccf7ad9f3b01ed8cd23b4467ab05f
윤석 이 <nuclearbomb280@gmail.com>
2025-07-03 17:34:40 +0900

    feature : 오브젝트 풀링 시스템 추가

---
+++ Assets/이윤석_작업/Scripts/ObjectPooling/ObjectPool.cs
@@ -0,0 +1,107 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectPool
+{
+    private int _maxPoolSize;
+    private GameObject _prefab = null;
+    private Vector3 _initialSpawnPoint;
+    private Quaternion _initialSpawnQuaternion;
+    private Transform _initialSpawnParent = null;
+    private Queue<GameObject> _gameObjectPool = null;
+
+    private GameObject CreateNewObject()
+    {
+        GameObject spawnedObject = GameObject.Instantiate(_prefab, _initialSpawnPoint, _initialSpawnQuaternion);
+
+        if (spawnedObject == null)
+        {
+            return null;
+        }
+
+        IPoolable poolableInterface = spawnedObject.GetComponent<IPoolable>();
+
+        spawnedObject.SetActive(false);
+        spawnedObject.transform.SetParent(_initialSpawnParent);
+
+        if (poolableInterface == null)
+        {
+            Debug.Log("오브젝트가 IPoolable을 가지고 있지 않습니다");
+            GameObject.Destroy(spawnedObject);
+            return null;
+        }
+
+        poolableInterface.SetPoolInstance(this);
+        return spawnedObject;
+    }
+
+    public GameObjectPool(GameObject prefab, Vector3 initSpawnPoint, Quaternion initialSpawnQuaternion, Transform initialSpawnParent,int initialPoolCapacity = 32, int maxPoolSize = 64)
+    {
+        _gameObjectPool = new Queue<GameObject>(initialPoolCapacity);
+        _maxPoolSize = maxPoolSize;
+        _prefab = prefab;
+        _initialSpawnPoint = initSpawnPoint;
+        _initialSpawnQuaternion = initialSpawnQuaternion;
+        _initialSpawnParent = initialSpawnParent;
+
+        for (int i = 0; i < initialPoolCapacity && i < maxPoolSize; i++)
+        {
+            GameObject spawnedObject = CreateNewObject();
+
+            if (spawnedObject == null)
+            {
+                continue;
+            }
+
+            _gameObjectPool.Enqueue(spawnedObject);
+        }
+    }
+
+    public void ReturnGameObject(GameObject returnedObject)
+    {
+        if (returnedObject == null)
+        {
+            return;
+        }
+
+        IPoolable poolableInterface = returnedObject.GetComponent<IPoolable>();
+
+        if (poolableInterface == null || !poolableInterface.ComparePoolInstance(this))
+        {
+            return;
+        }
+
+        if (_gameObjectPool.Count >= _maxPoolSize)
+        {
+            GameObject.Destroy(returnedObject);
+            return;
+        }
+
+        returnedObject.SetActive(false);
+        returnedObject.transform.SetParent(_initialSpawnParent);
+        returnedObject.transform.SetPositionAndRotation(_initialSpawnPoint, _initialSpawnQuaternion);
+        _gameObjectPool.Enqueue(returnedObject);
+    }
+
+    public GameObject GetGameObject()
+    {
+        GameObject selectedGameObject = null;
+
+        if (_gameObjectPool.Count <= 0)
+        {
+            selectedGameObject = CreateNewObject();
+        }
+        else
+        {
+            selectedGameObject = _gameObjectPool.Dequeue();
+        }
+
+        if (selectedGameObject != null)
+        {
+            selectedGameObject.SetActive(true);
+        }
+
+        return selectedGameObject;
+    }
+
+}
