0b56e6a8b60df3b7f1607930a231a1efabd69b72
MoccaHazel <wltn1937@gmail.com>
2025-08-26 15:01:36 +0900

    래버 이탈 오류 수정

--- Assets/Scripts/Inclinometer.cs
+++ Assets/Scripts/Inclinometer.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inclinometer : MonoBehaviour
 {
     [SerializeField] Transform targetTransform;
     [SerializeField] Camera frontCam;
     [SerializeField] Camera sideCam;
+
+    [Header("UI")]
+    [SerializeField] Text incinometerText;
+    float roll = 0;
+    float pitch = 0;
 
     Vector3 frontCamBasePos;
     Vector3 sideCamBasePos;
@@ -20,9 +26,23 @@ public class Inclinometer : MonoBehaviour
 
     private void Update()
     {
-        frontCam.transform.localPosition = targetTransform.localToWorldMatrix * frontCamBasePos;
-        frontCam.transform.LookAt(targetTransform.position + (Vector3)(targetTransform.localToWorldMatrix * new Vector3(frontCamBasePos.x, frontCamBasePos.y, 0)));
-        sideCam.transform.localPosition = targetTransform.localToWorldMatrix * sideCamBasePos;
-        sideCam.transform.LookAt(targetTransform.position + (Vector3)(targetTransform.localToWorldMatrix * new Vector3(0, sideCamBasePos.y, sideCamBasePos.z)));
+        CameraRot();
+        UpdateUI();
+    }
+
+    void CameraRot()
+    {
+            frontCam.transform.localPosition = targetTransform.localToWorldMatrix * frontCamBasePos;
+            frontCam.transform.LookAt(targetTransform.position + (Vector3)(targetTransform.localToWorldMatrix * new Vector3(frontCamBasePos.x, frontCamBasePos.y, 0)));
+            sideCam.transform.localPosition = targetTransform.localToWorldMatrix * sideCamBasePos;
+            sideCam.transform.LookAt(targetTransform.position + (Vector3)(targetTransform.localToWorldMatrix * new Vector3(0, sideCamBasePos.y, sideCamBasePos.z)));
+    }
+
+    void UpdateUI()
+    {
+        roll = 180>targetTransform.rotation.eulerAngles.z? targetTransform.rotation.eulerAngles.z : -(360 - targetTransform.rotation.eulerAngles.z);
+        pitch = 180 > targetTransform.rotation.eulerAngles.x ? targetTransform.rotation.eulerAngles.x : -(360 - targetTransform.rotation.eulerAngles.x);
+
+        incinometerText.text = "Roll \t: " + roll.ToString("F2") + "\n Pitch\t: " + pitch.ToString("F2");
     }
 }
